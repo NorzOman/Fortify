3d40a176118db2b34a75e0c3464eeaa2bacee8ec
NorzOman <omannorz@gmail.com>
2025-08-20 18:43:40 +0530

    fixed the code

--- FORTIFY-SERVER/FORTIFY-PHISHING-API/main.py
+++ FORTIFY-SERVER/FORTIFY-PHISHING-API/main.py
@@ -1,33 +1,27 @@
-from flask import Flask, request, jsonify
+# Format : main.py --message <message-goes-here>
+# Returns output of whether the message is phishing or not
+
 import joblib
+import sys
+import warnings
+
+# Supress warnings
+from sklearn.exceptions import InconsistentVersionWarning
+warnings.filterwarnings("ignore", category=InconsistentVersionWarning)
 
 model = joblib.load('phishing_detector_model.pkl')
 tfidf = joblib.load('tfidf_vectorizer.pkl')
-
-app = Flask(__name__)
 
 def predict_phishing(input_message):
     input_vector = tfidf.transform([input_message])
     prediction = model.predict(input_vector)
     return prediction[0]
 
-@app.route('/')
-def home():
-    return jsonify({'MESSEGE': 'Welcome to the Phishing Detection API!'}), 200
+if __name__ == "__main__":
+    if len(sys.argv) != 3 or sys.argv[1] != "--message":
+        print("Usage: main.py --message <message-goes-here>")
+        sys.exit(1)
 
-
-@app.route('/predict', methods=['POST'])
-def predict():
-    data = request.get_json()  # Get the JSON data from the request
-    message = data.get('message')  # Extract the message from the JSON data
-    
-    if message:
-        result = predict_phishing(message)
-        return jsonify({'message': message, 'prediction': result}), 200  # Return JSON response
-    else:
-        return jsonify({'error': 'No message provided'}), 400  # Handle missing message
-
-if __name__ == '__main__':
-    app.run(debug=False,host='0.0.0.0',port='5001')
-
-
+    message = sys.argv[2]
+    result = predict_phishing(message)
+    print(f"Result: {'Phishing' if result == 1 else 'Not phishing'}")
